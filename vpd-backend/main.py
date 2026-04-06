fd2e399f30455b9ac38fa8b2d91f516f2bab7a2d
Samyak <samyakgedam03@gmail.com>
2026-04-06 16:29:21 +0530

    Add env

--- vpd-backend/main.py
+++ vpd-backend/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi import HTTPException
 from db import get_connection, set_vpd_context
 
 app = FastAPI()
@@ -32,3 +33,43 @@ def get_employees(
     conn.close()
 
     return data
+
+@app.put("/employees/update-salary")
+def update_salary(
+    db_user: str,
+    db_password: str,
+    username: str,
+    role: str,
+    department: str,
+    location: str
+):
+    context = {
+        "username": username,
+        "role": role,
+        "department": department,
+        "location": location
+    }
+
+    try:
+        conn = get_connection(db_user, db_password)
+        set_vpd_context(conn, context)
+
+        cur = conn.cursor()
+
+        cur.execute("""
+            UPDATE employees
+            SET salary = salary + 500
+            WHERE department = %s
+        """, (department,))
+
+        rows_updated = cur.rowcount
+
+        conn.commit()
+
+        cur.close()
+        conn.close()
+
+        return {"message": f"{rows_updated} rows updated"}
+
+    except Exception as e:
+        raise HTTPException(status_code=403, detail=str(e))
